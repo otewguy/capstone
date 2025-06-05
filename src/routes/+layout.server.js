f75609f48496a2baaa937eaf5f4ed67225763499
unknown <hermanchitra.ut@gmail.com>
2025-06-05 23:02:51 +0700

    5Jun25b

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,8 +2,10 @@ import prisma from '$lib/prisma.server.js';
 
 export const load = async({locals: {safeGetSession}, cookies}) => {
     const {session, user} = await safeGetSession();
-    const profile = await prisma.profile.findUnique({
-        where: {userId: user.id}
-    });
-    return {session, role: profile.role, cookies: cookies.getAll()};
+    if (user) {
+        const profile = await prisma.profile.findUnique({
+            where: {userId: user.id}
+        });
+    }
+    return {session, role: profile?.role ?? null, cookies: cookies.getAll()};
 }
